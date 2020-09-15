472986c64b5d620837a053d1ff40d5b58c759570
Rui <ruimobile2012@gmail.com>
2020-09-15 01:30:02 -0700

    morgan's tiny api used. success

--- part3/index.js
+++ part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { response } = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
