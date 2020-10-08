41c89ce05530fcf99850a4e8635d9511d74b746d
Rui <ruimobile2012@gmail.com>
2020-10-08 01:01:34 -0700

    removed commented out parts. All refactored with working backend

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,32 +1,3 @@
-/*
-const config = require('./utils/config')
-// const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-// const Blog = require('./models/bloglist')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const bloglistRouter = require('./controllers/bloglist')
-
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-// default page... replace with build when adding front end
-app.get('/', (req, res) => res.send('<h1>No front-end until part5</h1>'))
-
-app.use('/api/blogs', bloglistRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
-})
-*/
-
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
--- part4/bloglist/models/bloglist.js
+++ part4/bloglist/models/bloglist.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose')
 
-// const mongoUrl = process.env.MONGODB_URI
-
-// console.log('connecting to: ', mongoUrl)
-
-// mongoose.connect(mongoUrl, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-// })
-//     .then(() => console.log('connected to MongoDB'))
-//     .catch((error) => console.error('error connecting to MongoDB: ', error.message))
-
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
