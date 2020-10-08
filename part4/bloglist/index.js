9016bf1ba630f033e00ed6f7993c9ab65b09dfe6
Rui <ruimobile2012@gmail.com>
2020-10-07 17:16:08 -0700

    able to get by individual id/entry

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -11,25 +11,38 @@ app.use(express.json())
 // default page
 app.get('/', (req, res) => res.send('<h1>No front-end until part5</h1>'))
 
-// view database conent
-app.get('/api/blogs', (request, response) => {
+// view entire DB conent
+app.get('/api/blogs', (req, res) => {
     Blog
         .find({})
-        .then(blogs => {
-            response.json(blogs)
+        .then(blogs => res.json(blogs))
+})
+
+// view individual DB content
+app.get('/api/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id)
+        .then(blog => {
+            if (blog)
+                res.json(blog)
+            else
+                res.status(404).end()
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).end()
         })
 })
 
 // add DB entry
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+app.post('/api/blogs', (req, res) => {
+    const blog = new Blog(req.body)
 
     blog
         .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+        .then(result => res.status(201).json(result))
 })
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
