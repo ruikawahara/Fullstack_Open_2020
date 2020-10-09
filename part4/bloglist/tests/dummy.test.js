c942c18d8c3761d888c7cb2ba927bb4c707ca930
Rui <ruimobile2012@gmail.com>
2020-10-09 03:33:25 -0700

    unit test for totalLikes works for blogs with 0 and 1 entry

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -4,4 +4,5 @@ test('dummy returns one', () => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,6 +6,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+// get sum of total likes from blog
+const totalLikes = (blogs) => {
+    // 0 or 1 blog entry
+    if (blogs.length < 2) {
+        return blogs.length === 0
+            ? blogs.length
+            : blogs[0].likes
+    }
+
+    // if it has multiple blogs, return total likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
