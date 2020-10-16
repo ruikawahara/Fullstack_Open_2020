6321cc356048b28efbaec6fb255136ed85d4dfc7
Rui <ruimobile2012@gmail.com>
2020-10-16 00:16:58 -0700

    moved mock DB to different file. Passed regression test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,51 +3,13 @@ const mongooose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper_blog')
 const Blog = require('../models/bloglist')
-
-const initialBlogs = [
-    {
-        title: 'React patterns',
-        author: 'Michael Chan',
-        url: 'https://reactpatterns.com/',
-        likes: 7
-    },
-    {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5
-    },
-    {
-        title: 'Canonical string reduction',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-        likes: 12
-    },
-    {
-        title: 'First class tests',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
-        likes: 10
-    },
-    {
-        title: 'TDD harms architecture',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-        likes: 0
-    },
-    {
-        title: 'Type wars',
-        author: 'Robert C. Martin',
-        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-        likes: 2
-    }
-]
 
 beforeEach(async () => {
     await Blog.deleteMany({})
 
-    for (let blog of initialBlogs) {
+    for (let blog of helper.initialBlogs) {
         const blogObj = new Blog(blog)
         await blogObj.save()
     }
@@ -63,7 +25,7 @@ describe.only('GET Request - ALL', () => {
 
     test('all blogs are returned', async () => {
         const res = await api.get('/api/blogs')
-        expect(res.body).toHaveLength(initialBlogs.length)
+        expect(res.body).toHaveLength(helper.initialBlogs.length)
     })
 
     test('have specific info within returned blogs', async () => {
